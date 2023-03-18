d0c8f366d111ecebc1b28f65e8b950a59d88a0e1
gtraiano <gtraiano@gmail.com>
2023-03-18 20:25:55 +0200

    replaced multiple identical return statements with break
    placed common statement outside switch block

--- src/state/reducers/chain.ts
+++ src/state/reducers/chain.ts
@@ -15,34 +15,34 @@ export const reducer = <T>(state: ChainState<T>, action: ChainAction<T>): ChainS
             // chain actions
             case ChainActionType.add:
                 action.payload.node && state.chain.add(action.payload.node, action.payload.index);
-                return { chain: state.chain, lastChangedOn: Date.now() };
+                break;
             
             case ChainActionType.remove:
                 state.chain.remove(action.payload?.index);
-                return { chain: state.chain, lastChangedOn: Date.now() };
+                break;
 
             case ChainActionType.replace:
                 action.payload.index !== undefined
                 && (state.chain.nodes[action.payload.index] as ChainNode<T>) !== undefined
                 && action.payload.node !== undefined
                 && (state.chain.nodes[action.payload.index] = action.payload.node);
-                return { chain: state.chain, lastChangedOn: Date.now() };
+                break;
     
             case ChainActionType.exchange:
                 action.payload.from !== undefined && action.payload.to !== undefined
                 && state.chain.swap(action.payload.from, action.payload.to);
-                return { chain: state.chain, lastChangedOn: Date.now() };
+                break;
     
             case ChainActionType.move:
                 action.payload.from !== undefined && action.payload.to !== undefined
                 && state.chain.move(action.payload.from, action.payload.to);
-                return { chain: state.chain, lastChangedOn: Date.now() };
+                break;
             
             // node actions
             case ChainActionType.bypass:
                 action.payload.index !== undefined && (action.payload.value !== undefined)
                 && ((state.chain.nodes[action.payload.index] as ChainNode<T>).bypass = action.payload.value as boolean);
-                return { chain: state.chain, lastChangedOn: Date.now() };
+                break;
     
             case ChainActionType.setOffset:
                 action.payload.index !== undefined
@@ -54,7 +54,7 @@ export const reducer = <T>(state: ChainState<T>, action: ChainAction<T>): ChainS
                         [action.payload.target] as unknown as GenericFunction<T>)
                             ?.setOffset(action.payload.value as T)
                 );
-                return { chain: state.chain, lastChangedOn: Date.now() };
+                break;
             
             case ChainActionType.setFrequency:
                 action.payload.index !== undefined
@@ -66,7 +66,7 @@ export const reducer = <T>(state: ChainState<T>, action: ChainAction<T>): ChainS
                         [action.payload.target] as unknown as PeriodicFunction)
                             ?.setFrequency(action.payload.value as number)
                 );    
-                return { chain: state.chain, lastChangedOn: Date.now() };
+                break;
 
             case ChainActionType.setPhase:
                 action.payload.index !== undefined
@@ -78,7 +78,7 @@ export const reducer = <T>(state: ChainState<T>, action: ChainAction<T>): ChainS
                         [action.payload.target] as unknown as PeriodicFunction)
                             ?.setPhase(action.payload.value as number)
                 );    
-                return { chain: state.chain, lastChangedOn: Date.now() };
+                break;
                 
             case ChainActionType.setAmplitude:
                 action.payload.index !== undefined
@@ -90,11 +90,12 @@ export const reducer = <T>(state: ChainState<T>, action: ChainAction<T>): ChainS
                         [action.payload.target] as unknown as PeriodicFunction)
                             ?.setAmplitude(action.payload.value as number)
                 );
-                return { chain: state.chain, lastChangedOn: Date.now() };
+                break;
     
             default:
                 return state;
         }
+        return { chain: state.chain, lastChangedOn: Date.now() };
     }
     catch(e: any) {
         console.error(`Error in action ${action.type}: ${e.message}`)
