907acde2cdf0ee96aec245ca7a7d3be18cacffb2
gtraiano <gtraiano@gmail.com>
2023-04-28 13:53:47 +0300

    added tests for log, exp and hypot functions

--- src/tests/unit/functions/arithmetic.test.ts
+++ src/tests/unit/functions/arithmetic.test.ts
@@ -15,7 +15,13 @@ describe('ArithmeticFunction', () => {
         [ArithmeticOperatorSymbol.EQ]: input,
         [ArithmeticOperatorSymbol.SQRT]: Math.sqrt(input),
         [ArithmeticOperatorSymbol.CBRT]: Math.cbrt(input),
-        [ArithmeticOperatorSymbol.ABS]: Math.abs(input)
+        [ArithmeticOperatorSymbol.ABS]: Math.abs(input),
+        [ArithmeticOperatorSymbol.E_EXP]: Math.exp(input),
+        [ArithmeticOperatorSymbol.E_EXP_M1]: Math.expm1(input),
+        [ArithmeticOperatorSymbol.HYPOT]: Math.hypot(input),
+        [ArithmeticOperatorSymbol.LN]: Math.log(input),
+        [ArithmeticOperatorSymbol.LOG_10]: Math.log10(input),
+        [ArithmeticOperatorSymbol.LOG_2]: Math.log2(input)
     };
 
     const expectedOffset: { [key in ArithmeticOperatorSymbol]: number } = {
@@ -27,7 +33,13 @@ describe('ArithmeticFunction', () => {
         [ArithmeticOperatorSymbol.EQ]: input + offset,
         [ArithmeticOperatorSymbol.SQRT]: Math.sqrt(input + offset),
         [ArithmeticOperatorSymbol.CBRT]: Math.cbrt(input + offset),
-        [ArithmeticOperatorSymbol.ABS]: Math.abs(input + offset)
+        [ArithmeticOperatorSymbol.ABS]: Math.abs(input + offset),
+        [ArithmeticOperatorSymbol.E_EXP]: Math.exp(input + offset),
+        [ArithmeticOperatorSymbol.E_EXP_M1]: Math.expm1(input + offset),
+        [ArithmeticOperatorSymbol.HYPOT]: Math.hypot(input + offset),
+        [ArithmeticOperatorSymbol.LN]: Math.log(input + offset),
+        [ArithmeticOperatorSymbol.LOG_10]: Math.log10(input + offset),
+        [ArithmeticOperatorSymbol.LOG_2]: Math.log2(input + offset)
     };
 
     const funcs = Object.values(generator).map(g => g());
@@ -50,7 +62,7 @@ describe('ArithmeticFunction', () => {
                 const calc = await f.calculate(input);
                 expect(calc).toBeDefined();
                 expect(calc).toBeTypeOf('number');
-                expect(calc).toBe(expectedOffset[f.getSymbol()]);
+                expect(calc).toBe(expectedOffset[f.getSymbol() as ArithmeticOperatorSymbol]);
             });
         }
 
@@ -73,13 +85,13 @@ describe('ArithmeticFunction', () => {
                 const calc = await f.calculate(input);
                 expect(calc).toBeDefined();
                 expect(calc).toBeTypeOf('number');
-                expect(calc).toBe(expected[f.getSymbol()]);
+                expect(calc).toBe(expected[f.getSymbol() as ArithmeticOperatorSymbol]);
 
                 f.setOffset(offset);
                 const calcOffset = await f.calculate(input);
                 expect(calcOffset).not.toBe(calc);
                 expect(calcOffset).toBeTypeOf('number');
-                expect(calcOffset).toBe(expectedOffset[f.getSymbol()]);
+                expect(calcOffset).toBe(expectedOffset[f.getSymbol() as ArithmeticOperatorSymbol]);
             });
         }
 
