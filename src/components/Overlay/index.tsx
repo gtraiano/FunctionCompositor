fc90b6871047050ecf31b4dee1312fb0240ec4b8
gtraiano <gtraiano@gmail.com>
2023-05-02 21:39:20 +0300

    - made onClose optional - made close button render only if onClose is defined - moved .no-overflow to component's css module

--- src/components/Overlay/index.tsx
+++ src/components/Overlay/index.tsx
@@ -4,19 +4,23 @@ import style from './style.module.css'
 interface OverlayProps {
     children: React.ReactNode,
     show: boolean,
-    onClose: () => void
+    onClose?: () => void
 }
 
 const Overlay = ({ children, show, onClose }: OverlayProps) => {
     useEffect(() => {
-        document.documentElement.classList[show ? 'add' : 'remove']('no-overflow');
+        document.documentElement.classList[show ? 'add' : 'remove'](style['no-overflow']);
     }, [show]);
     
     if(!show) return null;
 
     return (
-        <div className={style['overlay']} tabIndex={0} onKeyDown={e => { e.key === 'Escape' && onClose()}}>
-            <span className={style['close-btn']} onClick={onClose} />
+        <div
+            className={style['overlay']}
+            tabIndex={0}
+            onKeyDown={e => { e.key === 'Escape' && onClose && onClose() }}
+        >
+            {onClose && <span className={style['close-btn']} onClick={onClose} />}
             {children}
         </div>
     );
